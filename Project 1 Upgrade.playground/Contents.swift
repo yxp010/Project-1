c6c67177dd7d8d89557ac91da3bfdb07f2f9252a
KevinPPPP <40484345+yxp010@users.noreply.github.com>
2018-09-16 14:17:47 -0500

    Final Draft

--- Project 1 Upgrade.playground/Contents.swift
+++ Project 1 Upgrade.playground/Contents.swift
@@ -51,15 +51,37 @@ var letters = [String]()
 // It also automatically print out individual letter for each player and print out the average height of each team.
 func divideTeams(playerList: [[String: Any]]) {
     
+    var allPlayers = playerList
+    
+    // Use a simple Bubble sort to swap all players positions.
+    // Reposition the players from the shortest to tallest.
+    var swapped = false
+    var turn = 1
+    if allPlayers.count > 1 {
+        repeat {
+            swapped = false
+            for i in 0...allPlayers.count - 1 - turn {
+                if let player1 = allPlayers[i][Height] as? Double, let player2 = allPlayers[i + 1][Height] as? Double {
+                    if player1 > player2 {
+                        allPlayers[i...i + 1] = [allPlayers[i + 1], allPlayers[i]]
+                        swapped = true
+                    }
+                }
+            }
+            turn += 1
+        } while swapped
+    }
+    
     // Create two player list Array, be ready to store divided experienced and inexperienced players.
     var experiencedTeam = [[String: Any]]()
     var inexperiencedTeam = [[String: Any]]()
     
     // Create a for-loop to iterate over the playerList to seperate experienced and inexperienced players,
-    // and append them to the Arraies experiencedTeam and noExperiencedTeam created above.
-    for player in playerList{
+    // and append them to the Arraies that are experiencedTeam and noExperiencedTeam created above.
+    // Each temporary group, experiencedTeam and inexperiencedTeam, has the order that is from shortest to tallest.
+    for player in allPlayers {
         if let experienced = player[soccerExperience] as? Bool{
-            if experienced{
+            if experienced {
                 experiencedTeam.append(player)
             } else {
                 inexperiencedTeam.append(player)
@@ -67,86 +89,77 @@ func divideTeams(playerList: [[String: Any]]) {
         }
     }
     
-    // Create a for-loop(Bubble Sort) to swap the players' positions in the Array; make the tallest go to the last position, and
-    // the shortest player goes to first position in the experiencedTeam Array.
-    var swapped = false
-    if experiencedTeam.count >= 2 {
-        repeat {
-            var swapped = false
-            for i in 0...experiencedTeam.count - 2 {
-                for j in 0...experiencedTeam.count - i - 2 {
-                    if let player1 = experiencedTeam[j][Height] as? Double, let player2 = experiencedTeam[j + 1][Height] as? Double {
-                        if player1 > player2 {
-                            experiencedTeam[j...j + 1] = [experiencedTeam[j + 1], experiencedTeam[j]]
-                            swapped = true
-                        }
-                    }
-                }
-            }
-        } while swapped
-    }
-    
-    // Create a for-loop(Bubble Sort) to swap the players' positions in the Array; make the shortest go to the last position, and
-    // the tallest player goes to first position in the inexperiencedTeam Array.
-    if inexperiencedTeam.count >= 2 {
-        repeat {
-            swapped = false
-            for i in 0...inexperiencedTeam.count - 2 {
-                for j in 0...inexperiencedTeam.count - i - 2 {
-                    if let player1 = inexperiencedTeam[j][Height] as? Double, let player2 = inexperiencedTeam[j + 1][Height] as? Double {
-                        if player1 < player2 {
-                            inexperiencedTeam[j...j + 1] = [inexperiencedTeam[j + 1], inexperiencedTeam[j]]
-                            swapped = true
-                        }
-                    }
-                }
-            }
-        } while swapped
-    }
-    
     // Create a variable named 'teamTurn', used as index in later function to iterate over the allTeams to append one player a time into one of the three teams.
-    var teamturn = 0
+    var teamTurn = 0
     
-    // This function is to make index back to 0 after 2, so the index should always be 0, 1, 2.
-    func teamTurn() {
-        teamturn += 1
-        if teamturn > 2 {
-            teamturn = 0
+    // This helper function is to make index back to 0 after 2, so the index should always be 0, 1, 2.
+    func chageTeamTurn() {
+        teamTurn += 1
+        if teamTurn > 2 {
+            teamTurn = 0
         }
     }
     
-    // This function takse two parameters of [[String: Any]] type, it will use inexperiencedTeam and experiencedTeam as parameters.
-    // This function will iterate the inexperiencedTeam and experiencedTeam. While the temporary inexpeirenced and experienced group still has players,
-    // the while loop will append one player at a time into the allTeams list start from index 0.
-    // Appending one player at a time is to making sure that the number of players and number of experienced players each team are balanced as closed as possible.
-    // Since the experienced team sorted from shortest player to tallest and inexperienced team is tallest to shortest, and index for both while-loop starts from 0, three teams average height will be balanced enough.
-    func appendPlayersFrom(inexTeam: [[String: Any]], exTeam: [[String: Any]]) {
-        var teamInEx = inexTeam
-        var teamEx = exTeam
-        while teamInEx.count > 0 {
-            allTeams[teamturn].append(teamInEx.remove(at: 0))
-            teamTurn()
+    // Create a boolean variable to be used as a condition to switch the order the function appending players.
+    // If the first time appeding players to three teams is from the shortest to tallest, the next time will be from the tallest to shortest.
+    var fromShortest = true
+    
+    // Create a helper function uses experiencedTeam or inexperiencedTeam as parameter.
+    // This function will append players from the temporary group into each team one player at a time until the teamporary group is empty.
+    // Only appending one player at a time is because we need to balance number of players in each team no matter the total number of players is dividable by number of teams or not.
+    // If fromShortest is true, this function will append players from the shortest to tallest. If fromShortest is false, the function will append players from tallest to shortest. It makes average heights of three teams are pretty closed.
+    // If after we ran this function and fromShortest remained false, next time we call this function will append players from tallest to shortest due to fromShortest is false.
+    // The function needs to check every time if there is enough players in the tempGroup after it appends a player into a team becasue there might be some situations that the number of players in the original tempGroup is not evenly dividable by three teams.
+    func appendPlayersFrom(temporaryGroup: [[String: Any]]) {
+        var tempGroup = temporaryGroup
+        while tempGroup.count > 0 {
+            if fromShortest {
+                allTeams[teamTurn].append(tempGroup.remove(at: 0))
+                chageTeamTurn()
+                if tempGroup.count > 0 {
+                    allTeams[teamTurn].append(tempGroup.remove(at: 0))
+                    chageTeamTurn()
+                    if tempGroup.count > 0 {
+                        allTeams[teamTurn].append(tempGroup.remove(at: 0))
+                        chageTeamTurn()
+                    }
+                }
+                fromShortest = false
+            } else if fromShortest == false {
+                allTeams[teamTurn].append(tempGroup.remove(at: tempGroup.count - 1))
+                chageTeamTurn()
+                if tempGroup.count > 0 {
+                    allTeams[teamTurn].append(tempGroup.remove(at: tempGroup.count - 1))
+                    chageTeamTurn()
+                    if experiencedTeam.count > 0 {
+                        allTeams[teamTurn].append(tempGroup.remove(at: tempGroup.count - 1))
+                        chageTeamTurn()
+                    }
+                }
+                fromShortest = true
+            }
         }
-        teamturn = 0
-        while teamEx.count > 0 {
-            allTeams[teamturn].append(teamEx.remove(at: 0))
-            teamTurn()
-        }   
     }
     
-    appendPlayersFrom(inexTeam: inexperiencedTeam, exTeam: experiencedTeam)
+    // Call appendPlayersFrom helper function twice using experiencedTeam and inexperiencedTeam respectively.
+    // After this, each team in allPlayers Array will have closed average height and balanced number of experienced players.
+    appendPlayersFrom(temporaryGroup: experiencedTeam)
     
-    // Create three dictionaries to have team names, practice date and time, and assign divided players in allTeams array to each team.
-    // So each divided team in allTeams list has a name and practice date and time now.
+    // Since experiencedTeam has 9 players and we have three teams, the function will append players from the tallest to shortest due to fromShortest is false now.
+    appendPlayersFrom(temporaryGroup: inexperiencedTeam)
+    
+    // Create three dictionaries to have team names, practice date and time, and assign a team in allTeams Array to each team.
+    // So each team in allTeams Array has a name and practice date and time now.
     let teamSharks: [String: Any] = [teamName: "Team Sharks", practiceDateAndTime: "March 17, 3pm", teamPlayers: allTeams[0]]
     let teamDragons: [String: Any] = [teamName: "Team Dragon", practiceDateAndTime: "March 17, 1pm", teamPlayers: allTeams[1]]
     let teamRaptors: [String: Any] = [teamName: "Team Raptors", practiceDateAndTime: "March 18, 1pm", teamPlayers: allTeams[2]]
     
-    // Add all three teams' information into a new array named teamList.
-    // This array will be used later when printng out individual letters.
-    var teamList = [teamSharks, teamRaptors, teamDragons]
+    // Add all three teams' information into a new Array named teamList.
+    // This array will be iterated later to print out individual letters.
+    let teamList = [teamSharks, teamRaptors, teamDragons]
 
     // Create a variable as a index in the for-loop when we iterate the letters Array to print out each letter.
+    // Actually I do not understand why I need to append each letter into letters array, but it is a requirement. If not, I won't need this variable.
     var letterIndex = 0
     
     // Create a nested for-loop, iterate each team in teamList and print out the individul letter for each player.
@@ -166,32 +179,24 @@ func divideTeams(playerList: [[String: Any]]) {
         }
     }
     
-    // Create a function inside, that takes teamList array as the parameter.
-    // The function will iterate all players of each team of teamList, and calucate their average height.
-    // it will also print out the three teams' average heights and their team name.
-    func calculateAverageHeight(teamList: [[String: Any]]) {
-        var sumOfHeight: Double = 0
-        var averageHeight: Double
-        for team in teamList {
-            if let players = team[teamPlayers] as? [[String: Any]], let teamName = team[teamName] as? String {
-                for player in players {
-                    if let playerHeight = player[Height] as? Double {
-                        sumOfHeight += playerHeight
-                    }
+    // Create a nested for-loop to calculate all three teams average height and print it out.
+    var sumOfHeight: Double = 0
+    var averageHeight: Double
+    for team in teamList {
+        if let players = team[teamPlayers] as? [[String: Any]], let teamName = team[teamName] as? String {
+            for player in players {
+                if let playerHeight = player[Height] as? Double {
+                    sumOfHeight += playerHeight
                 }
-                averageHeight = sumOfHeight / Double(players.count)
-                print("\(teamName)'s average height is \(averageHeight).")
-                sumOfHeight = 0
             }
+            averageHeight = sumOfHeight / Double(players.count)
+            print("\(teamName)'s average height is \(averageHeight).")
+            sumOfHeight = 0
         }
     }
-    calculateAverageHeight(teamList: teamList)
 }
 
 divideTeams(playerList: players)
-
-
-
 
 
 
